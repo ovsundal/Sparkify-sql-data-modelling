0b0610ce9f453e8bd159e665718e844713d9c5b3
ove <osundal83@gmail.com>
2019-04-16 12:14:42 +0200

    restart project and reset all files based on reviewer feedback. Complete etl.ipynb

--- create_tables.py
+++ create_tables.py
@@ -1,146 +1,47 @@
 import psycopg2
-from sql_queries import \
-    drop_table_queries, create_table_queries, dim_songs_table_insert, dim_artists_table_insert, \
-    dim_time_table_insert, dim_users_table_insert, select_dim_songs_song_id, select_dim_artist_artist_id, \
-    fact_songplays_table_insert
+from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
-    """
-    connects to postgres default database, and drops (if exists) the sparkifydb and recreates it
-    """
-    try:
-        conn = psycopg2.connect("host=127.0.0.1 dbname=postgres user=postgres password=postgres")
-        conn.set_session(autocommit=True)
-        cur = conn.cursor()
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
-    # create a fresh instance of sparkify database with UTF-8 encoding
-        cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-        cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0 ")
-    except psycopg2.Error as e:
-        print('Error: Could not drop or create the database')
-        print(e)
-    finally:
-        conn.close()
-
-
-def connect_to_database():
-    try:
-        conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=postgres")
-        cur = conn.cursor()
-        conn.set_session(autocommit=True)
-
-        return cur, conn
-    except psycopg2.Error as e:
-        print('Error: Issue creating the database')
-        print(e)
+    # close connection to default database
+    conn.close()    
+    
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+    
+    return cur, conn
 
 
 def drop_tables(cur, conn):
-    try:
-        for query in drop_table_queries:
-            cur.execute(query)
-            conn.commit()
-    except psycopg2.Error as e:
-        print('Error: Issue dropping a table')
-        print(e)
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
 
 
 def create_tables(cur, conn):
-    try:
-        for query in create_table_queries:
-            cur.execute(query)
-            conn.commit()
-    except psycopg2.Error as e:
-        print('Error: Issue creating a table')
-        print(e)
-
-
-def insert_into_dim_songs_table(cur, conn, datalist):
-    try:
-        cur.execute(dim_songs_table_insert, datalist)
+    for query in create_table_queries:
+        cur.execute(query)
         conn.commit()
-    except psycopg2.Error as e:
-        print('Error while inserting')
-        print(e)
-
-
-def insert_into_dim_artists_table(cur, conn, datalist):
-    try:
-        cur.execute(dim_artists_table_insert, datalist)
-        conn.commit()
-    except psycopg2.Error as e:
-        print('Error while inserting')
-        print(e)
-
-
-def insert_into_dim_time_table(cur, conn, datalist):
-    try:
-        cur.execute(dim_time_table_insert, datalist)
-        conn.commit()
-    except psycopg2.Error as e:
-        print('Error while inserting')
-        print(e)
-
-
-def insert_into_dim_user_table(cur, conn, datalist):
-    try:
-        cur.execute(dim_users_table_insert, datalist)
-        conn.commit()
-    except psycopg2.Error as e:
-        print('Error while inserting')
-        print(e)
-
-
-def insert_into_fact_songplays_table(cur, conn, datalist):
-    try:
-        cur.execute(fact_songplays_table_insert, datalist)
-        conn.commit()
-    except psycopg2.Error as e:
-        print('Error while inserting')
-        print(e)
-
-
-def get_song_id(cur, conn, datalist):
-    try:
-        cur.execute(select_dim_songs_song_id, datalist)
-        conn.commit()
-
-        return cur.fetchone()
-
-    except psycopg2.Error as e:
-        print('Error while selecting')
-        print(e)
-
-
-def get_artist_id(cur, conn, datalist):
-    try:
-        cur.execute(select_dim_artist_artist_id, datalist)
-        conn.commit()
-
-        return cur.fetchone()
-
-    except psycopg2.Error as e:
-        print('Error while selecting')
-        print(e)
 
 
 def main():
-    """
-    Creates a clean instance of the sparkifydb with empty tables
-    """
-    try:
-        create_database()
-        cur, conn = connect_to_database()
+    cur, conn = create_database()
+    
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
 
-        drop_tables(cur, conn)
-        create_tables(cur, conn)
-    finally:
-        conn.close()
-        cur.close()
+    conn.close()
 
 
-# create database and tables
-main()
-
-
+if __name__ == "__main__":
+    main()
--- sql_queries.py
+++ sql_queries.py
@@ -1,46 +1,52 @@
 # DROP TABLES
-fact_songplay_table_drop = "DROP TABLE IF EXISTS fact_songplays"
-dim_user_table_drop = "DROP TABLE IF EXISTS dim_users"
-dim_songs_table_drop = "DROP TABLE IF EXISTS dim_songs"
-dim_artists_table_drop = "DROP TABLE IF EXISTS dim_artists"
-dim_time_table_drop = "DROP TABLE IF EXISTS dim_time"
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-fact_songplay_table_create = """
-CREATE TABLE IF NOT EXISTS fact_songplays
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays
 (
 songplay_id serial PRIMARY KEY,
-start_time time NOT NULL,
-user_id int NOT NULL,
+timestamp bigint NOT NULL,
+user_id int NULL,
 level varchar(255) NOT NULL,
-song_id varchar(255) NOT NULL,
-artist_id varchar(255) NOT NULL,
+song_id varchar(255) NULL,
+artist_id varchar(255) NULL,
 session_id int NOT NULL,
 location varchar(255),
 user_agent varchar(255)
 );
-"""
-dim_user_table_create = """
-CREATE TABLE IF NOT EXISTS dim_users 
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users
 (
 user_id varchar(255) PRIMARY KEY,
 first_name varchar(255),
 last_name varchar(255),
 gender char,
 level varchar(255) NOT NULL
-);"""
-dim_songs_table_create = """
-CREATE TABLE IF NOT EXISTS dim_songs
+);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs
 (
 song_id varchar(255) PRIMARY KEY,
 title varchar(255) NOT NULL,
 artist_id varchar(255) NOT NULL,
-year smallint NOT NULL,
-duration numeric NOT NULL
+year int NOT NULL,
+duration float NOT NULL
 );
-"""
-dim_artists_table_create = """
-CREATE TABLE IF NOT EXISTS dim_artists
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists
 (
 artist_id varchar(255) NOT NULL,
 name varchar(255) NOT NULL,
@@ -48,51 +54,65 @@ location varchar(255) NOT NULL,
 latitude real NULL,
 longitude real NULL
 );
-"""
-dim_time_table_create = """
-CREATE TABLE IF NOT EXISTS dim_time
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time
 (
-start_time timestamp,
-hour smallint NOT NULL,
-day smallint NOT NULL,
-week smallint NOT NULL,
-month smallint NOT NULL,
-year smallint NOT NULL,
-weekday varchar(255) NOT NULL
+timestamp timestamp,
+hour int NOT NULL,
+day int NOT NULL,
+week int NOT NULL,
+month int NOT NULL,
+year int NOT NULL,
+weekday int NOT NULL
 );
-"""
+""")
 
-# INSERTS
-dim_songs_table_insert = "INSERT INTO dim_songs(song_id, title, artist_id, year, duration) " \
-                         "VALUES(%s,%s,%s,%s,%s);"
-dim_artists_table_insert = "INSERT INTO dim_artists(artist_id, name, location, latitude, longitude) " \
-                           "VALUES(%s,%s,%s,%s,%s);"
-dim_time_table_insert = "INSERT INTO dim_time(start_time, hour, day, week, month, year, weekday) " \
-                        "VALUES(%s,%s,%s,%s,%s,%s,%s);"
-dim_users_table_insert = "INSERT INTO dim_users(user_id, first_name, last_name, gender, level)" \
-                         "VALUES(%s,%s,%s,%s,%s);"
-fact_songplays_table_insert = "INSERT INTO fact_songplays(start_time, user_id, level, song_id, artist_id, " \
-                              "session_id, location, user_agent)" \
-                         "VALUES(%s,%s,%s,%s,%s,%s,%s,%s);"
+# INSERT RECORDS
 
-# SELECT QUERIES
-select_dim_songs_song_id = "SELECT song_id FROM dim_songs WHERE dim_songs.title = %s AND dim_songs.duration = %s;"
-select_dim_artist_artist_id = "SELECT DISTINCT artist_id FROM dim_artists WHERE dim_artists.name = %s;" \
+songplay_table_insert = ("""
+INSERT INTO songplays(timestamp, user_id, level, song_id, artist_id,
+session_id, location, user_agent) 
+VALUES(%s,%s,%s,%s,%s,%s,%s,%s);
+""")
+
+user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level)
+VALUES(%s,%s,%s,%s,%s)
+ON CONFLICT(user_id) DO UPDATE
+    SET first_name = excluded.first_name,
+        last_name = excluded.last_name,
+        gender = excluded.gender,
+        level = excluded.level;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs(song_id, title, artist_id, year, duration)
+VALUES(%s,%s,%s,%s,%s);
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists(artist_id, name, location, latitude, longitude)
+VALUES(%s,%s,%s,%s,%s);
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time(timestamp, hour, day, week, month, year, weekday)
+VALUES(%s,%s,%s,%s,%s,%s,%s);
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT s.song_id, a.artist_id FROM songs AS s
+LEFT OUTER JOIN artists AS a ON s.artist_id = a.artist_id
+WHERE s.duration = %s AND s.title = %s AND a.name = %s;
+""")
 
 
 # QUERY LISTS
-drop_table_queries = [
-    fact_songplay_table_drop,
-    dim_user_table_drop,
-    dim_songs_table_drop,
-    dim_artists_table_drop,
-    dim_time_table_drop
-]
-create_table_queries = [
-    fact_songplay_table_create,
-    dim_user_table_create,
-    dim_songs_table_create,
-    dim_artists_table_create,
-    dim_time_table_create
-]
 
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
